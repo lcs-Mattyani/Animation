7f15501bbb08ea3351f8a14e21c3b53238fbd250
Mariam Attyani <Mattyani@lcs.on.ca>
2021-10-14 09:37:42 -0400

    working on thing for t shirt almost done finished at around arrow 9

--- Playground.playground/Pages/Empty Canvas 3.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 3.xcplaygroundpage/Contents.swift
@@ -110,13 +110,96 @@ p.goto(dx: -80, dy: 0)
 p.drawTo(dx: -60, dy: -80)
 p.drawTo(dx: 80, dy: 0)
 
-//ARROW TIME #7
+//ARROW TIME #8
 
-p.goto(dx: -80, dy: 0)
-p.turn(degrees: 129)
-p.addLine(distance: 50)
-
+p.goToOrigin()
+p.goto(dx: -140, dy: -80)
 p.drawTo(dx: -60, dy: 80)
+
+p.drawTo(dx: -35, dy: -40)
+p.drawTo(dx: 0, dy: 160)
+p.drawTo(dx: 125, dy: 0)
+p.drawTo(dx: -30, dy: -40)
+
+//ARROW TIME NUMBER 11 BESTIE
+
+p.goto(dx: 30, dy: 40)
+canvas.fillColor = .black
+p.beginFill()
+p.drawTo(dx: 30, dy: 40)
+p.drawTo(dx: -50, dy: 0)
+
+// Little triangle thing like that makes the arrow 3d yknow this one is for number 11
+
+p.drawTo(dx: 20, dy: -40)
+
+p.endFill()
+
+//Ok returning to the actual arrow #11 again
+
+p.goToOrigin()
+p.goto(dx: -130, dy: 160)
+p.drawTo(dx: 150, dy: 80)
+p.drawTo(dx: 30, dy: -80)
+p.drawTo(dx: -50, dy: 0)
+
+
+//OK ITS ARROW 10 NOW
+
+p.goToOrigin()
+
+p.goto(dx: 50, dy: 160)
+
+//doing the little 3d thing to fill
+
+p.fillColor = .white
+p.beginFill()
+
+
+
+p.drawTo(dx: 40, dy: 0)
+p.drawTo(dx: -40, dy: 45)
+p.drawTo(dx: 0, dy: -45)
+
+p.endFill()
+
+//back to arrow
+
+p.goto(dx: 0, dy: 45)
+p.drawTo(dx: 140, dy: -85)
+p.drawTo(dx: -20, dy: -80)
+p.drawTo(dx: -30, dy: 40)
+
+
+// Arrow #3 timeee
+
+p.drawTo(dx: 60, dy: -80)
+p.drawTo(dx: 35, dy: 50)
+p.drawTo(dx: 0, dy: -170)
+p.drawTo(dx: -125, dy: 0)
+p.drawTo(dx: 30, dy: 40)
+
+//3d thing for arrow # 3
+
+p.goto(dx: 95, dy: 130)
+
+p.drawTo(dx: -65, dy: -10)
+
+//Arrow #9
+
+p.goToOrigin()
+
+p.goto(dx: 110, dy: -120)
+
+p.drawTo(dx: -30, dy: -40)
+
+p.drawTo(dx: 50, dy: 0)
+
+p.drawTo(dx: -20, dy: 40)
+
+
+
+
 
 
 /*:
