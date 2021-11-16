ea29bdea9229fb6628e6d50882348bb606c2f1f2
Mariam Attyani <Mattyani@lcs.on.ca>
2021-11-16 09:57:47 -0500

    Made the panda head w/ the canvas command coloured everything that was done in need to do the body next

--- Playground.playground/Pages/Empty Canvas 9.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 9.xcplaygroundpage/Contents.swift
@@ -56,22 +56,145 @@ canvas.drawAxes(withScale: true, by: 20, color: .black)
 
  */
 
-// Begin writing your code below (you can remove the examples shown)
 
-// Draw a circle, using the canvas object directly
-canvas.drawEllipse(at: Point(x: 100, y: 100), width: 25, height: 25)
 
-// Draw a vertical line, up and to the left
-p.drawTo(dx: -25, dy: 50)
 
-// Go back to origin
-p.goToOrigin()
 
-// Change the pen color
-p.penColor = .red
 
-// Draw a curve, down and to the right
-p.addArc(radius: 50, angle: -45)
+
+//ear behind the panda head
+
+
+canvas.fillColor = .black
+
+canvas.drawEllipse(at: Point(x: 0, y: 78), width: 25, height: 30)
+
+// side view head of panda
+
+
+canvas.fillColor = .white
+canvas.drawShapesWithBorders = true
+canvas.drawEllipse(at: Point(x: 0, y: 40), width: 80, height: 80)
+
+//adding nose
+canvas.fillColor = .black
+canvas.drawEllipse(at:Point(x: -40, y: 35), width: 15, height: 20)
+
+//eyes
+
+canvas.drawEllipse(at: Point(x: -10, y: 50), width: 25, height: 27)
+
+canvas.fillColor = .white
+
+canvas.drawEllipse(at: Point(x: -15, y: 45), width: 10, height: 10)
+
+//doing the ears now
+
+canvas.fillColor = .black
+
+canvas.drawEllipse(at: Point(x: 20, y: 80), width: 25, height: 30)
+
+//ok so body do we want like standing/walking look or sitting im thinking sitting but lets try it out
+
+// ASK MR GORDON ABT ADDING A SMILE
+//MAYBE ADD A BLUSH OR SMTHG LATER OK
+
+// adding body (SITTING)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//p.addArc(radius: 40, angle: 360)
+//
+////adding nose of panda head
+//
+//
+//p.goToOrigin()
+//p.goto(dx: -40, dy: 25)
+//p.addArc(radius: 10, angle: 360)
+//
+//
+////adding eyes to the panda
+//
+//p.goToOrigin()
+//p.goto(dx: -10, dy: 40)
+//p.addArc(radius: 10, angle: 360)
+//
+////adding ears to the head
+//
+//p.goToOrigin()
+//p.goto(dx: 10, dy: 60)
+//p.addArc(radius: 15, angle: 270)
+//
+////body
+//
+//p.goToOrigin()
+//p.goto(dx: 90, dy: -10)
+//p.addArc(radius: 50, angle: -360)
+//
+////tail
+//
+//p.goToOrigin()
+//p.goto(dx: 85, dy: 0)
+//p.addArc(radius: 10, angle: 360)
+//
+////arms
+//
+//p.goToOrigin()
+//p.goto(dx: 20, dy: 0)
+//p.addArc(radius: 10, angle: -180)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*:
  ## Show the Live View
